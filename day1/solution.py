dd1896eafa23640f931308d6f3cd5c5368a5d2f0
Keith Choa <keithchoa@gmail.com>
2022-12-01 13:42:26 +0800

    day 1

---
+++ day1/solution.py
@@ -0,0 +1,14 @@
+import sys
+
+if __name__ == "__main__":
+    running_sum = 0
+    elves = []
+    for line in sys.stdin:
+        if(line == "\n"):
+            elves.append(running_sum)
+            running_sum = 0
+        else:
+            running_sum += int(line)
+    elves.sort()
+    print("Top Three:", sum(elves[-3:]))
+    
