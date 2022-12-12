b221e42780113790a0868f24409bc803ae27a28b
Keith Choa <keithchoa@gmail.com>
2022-12-12 13:25:23 +0800

    day 7 complete

--- day7/solution.py
+++ day7/solution.py
@@ -1,5 +1,7 @@
 import sys
 
+PART = 2
+
 class directory:
     def __init__(self, name):
         self.name = name
@@ -30,16 +32,24 @@ class directory:
 # Traverse down through root directory, keep count of total size
 #   If the file size is less than 100000, add it to the total size
 #   If the file size is greater than 100000, ignore it
-def process_tree(directory):
+def process_tree_part_1(directory):
     if directory.file_sizes < 100000:
         total_size = directory.file_sizes
     else:
         total_size = 0
 
     for child in directory.children:
-        total_size += process_tree(child)
+        total_size += process_tree_part_1(child)
 
     return total_size
+
+def process_tree_part_2(target_size, array, directory):
+    if directory.file_sizes > target_size:
+        array.append(directory.file_sizes)
+
+    for child in directory.children:
+        process_tree_part_2(target_size, array, child)
+    return array
 
 if __name__ == "__main__":
 
@@ -51,22 +61,12 @@ if __name__ == "__main__":
         line = line[:-1].split(" ")
         
         if processing_ls:
-            print("processing ls:", line)
             # process each line of the ls output.
             #   if it starts with "dir", process as a new directory (or ignore? we create one anyway for cd)
             #   if it starts with a number, add the number to the file size
-            
             if line[0].isdigit():
                 current_directory.add_file_size(int(line[0]))
             elif line[0] == "dir":
-                # child_exists = False
-                # for child in current_directory.get_children():
-                #     if child.name == line[1]:
-                #         child_exists = True
-                #         break
-                # if not child_exists:
-                #     new_directory = directory(line[1])
-                #     current_directory.add_child(new_directory)
                 continue
             else:
                 processing_ls = False
@@ -74,13 +74,10 @@ if __name__ == "__main__":
         if not processing_ls:
             # Process the cd command
             if line[0] == "$" and line[1] == "cd":
-                print(line)
                 if line[2] == "..":
-                    print(current_directory.name)
                     current_directory = current_directory.parent
                 else:
                     child_exists = False
-                    print(current_directory.name)
                     for child in current_directory.get_children():
                         if child.name == line[2]:
                             current_directory = child
@@ -92,13 +89,19 @@ if __name__ == "__main__":
                         current_directory = new_directory
             # Process the ls command
             elif line[0] == "$" and line[1] == "ls":
-                print(line)
                 processing_ls = True
     
     # root_directory.print_tree()
 
-    total_size = process_tree(root_directory)
-    print(total_size)
-
-
-    
+    if PART == 1:
+        total_size = process_tree_part_1(root_directory)
+        print(total_size)
+    else:
+        space_available = 70000000
+        unused_space = space_available - root_directory.file_sizes
+        print("target space:", 30000000 - unused_space)
+        
+        array = process_tree_part_2(30000000 - unused_space, [], root_directory)
+        array.sort()
+        print(array[0])
+        
